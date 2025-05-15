7f11d761a2c647e5a217ed9dfa1957c1cc31cb7f
IsmaelLebrun <ismael.lebrun@laplateforme.io>
2025-05-15 15:13:01 +0200

    affichage note en cours

--- trash-coach/app/page.js
+++ trash-coach/app/page.js
@@ -1,8 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
-import handler from "./api/route";
+import mysql from "mysql2/promise";
 
-export default function Home() {
-  handler;
-  return <p> hello world </p>;
+function htmlGenerate (response){
+  response.forEach(element => {
+    return <p value="element.note"></p>;
+  });
+};
+
+export default async function Home() {
+  let connexion = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "1234",
+      database: "trashCoach",
+  });
+  
+  try {
+    const response = await connexion.execute("SELECT * FROM notes");    
+    console.log(response[0]);
+    return htmlGenerate(response[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erreur serveur" });
+    return err;
+  } finally {
+    await connexion.end();
+  }
 }
