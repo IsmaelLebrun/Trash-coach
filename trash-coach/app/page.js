3520aa8cccff932e12d5d0dae9201f668f29a164
IsmaelLebrun <ismael.lebrun@laplateforme.io>
2025-05-16 11:23:07 +0200

    génération de note selon tes notes

--- trash-coach/app/page.js
+++ trash-coach/app/page.js
@@ -11,13 +11,54 @@ export default async function Home() {
       password: process.env.MYSQL_PASSWORD,
       database: "trashCoach",
   });
-    let goodNote = [];
-    let badNote = [];
+
+  let compliment = [
+    "tkt ca va aller",
+    "continue mon reuf",
+    "t'es sur la bonne voie",
+    "tout le mondre croit en toi",
+    "n'abandonne pas",
+    "persiste, continue, avance !"
+  ];
+  let rabaissement = [
+    "mange tes morts",
+    "sac à merde",
+    "t'es sérieux ? même le cadavre de ma grand-mère fait mieux",
+    "t'aurais pu mourir",
+    "ta tête de gargouille retourne lécher les boules de satan",
+    "tu t'es cru sur minecraft en peaceful ?",
+    "même les bots te trickshot",
+    "t'es mauvais jack, t'es mauvais"
+  ];
+
+  let message = "";
+
+  try{
+    const HttpResponse = await connexion.execute("SELECT effect FROM notes");
+    const response = HttpResponse[0];
+
+    let noteTotal = 0;
+    response.forEach((element)=> {
+      noteTotal = noteTotal + element;
+    });
+    const moyenne = noteTotal / response.length;
+    if(moyenne >= (response.lenght / 2)){
+      // moyenne bonne
+      let random = Math.floor(Math.random() * compliment.length);
+      message = compliment[random];
+    }else{
+      let random = Math.floor(Math.random() * rabaissement.length);
+      message = rabaissement[random];
+    }
+  }catch(err){
+    console.error("erreur server : " + err)
+  }
+
+  let goodNote = [];
+  let badNote = [];
   try {
     const HttpResponse = await connexion.execute("SELECT * FROM notes");
     const response = HttpResponse[0];
-
-
 
     response.forEach((element) => {
       if(element.effect > 2){
@@ -37,6 +78,7 @@ export default async function Home() {
   }
   return (
     <div className="notes-container flex flex-col lg:flex-row gap-6 p-4">
+      <h1>{message}</h1>  
       <div className="bad-notes bg-red-50 shadow-md rounded-2xl p-6 flex-1">
         <h2 className="text-xl font-semibold text-red-600 mb-4">Bad Notes</h2>
         {badNote.length > 0 ? (
